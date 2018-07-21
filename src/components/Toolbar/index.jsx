daa5386e1b27c531b92e984593ae171bf6304b80
Amy Lee <amy@seqmedia.com>
2018-07-21 08:16:43 -0700

    Toolbar should only show relevant controls

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -4,13 +4,32 @@
 import React from "react";
 import State from "../../State";
 import PatchUtils from "../../PatchUtils";
+import Util from "../../Util";
 
 import "./index.css";
 
-const handleExportPatches = () => {
-  const version = State.get("patchesversion");
-  if (!version) return;
+const handleBankKeyUp = e => {
+  const keycode = Util.key(e);
+  if (Util.isArrowUp(keycode)) {
+    PatchUtils.setBank(PatchUtils.getBank() + 1);
+  } else if (Util.isArrowDown(keycode)) {
+    PatchUtils.setBank(PatchUtils.getBank() - 1);
+  }
+};
 
+const handlePatchKeyUp = e => {
+  const keycode = Util.key(e);
+  if (Util.isArrowUp(keycode)) {
+    PatchUtils.setPatch(PatchUtils.getPatch() + 1);
+  } else if (Util.isArrowDown(keycode)) {
+    PatchUtils.setPatch(PatchUtils.getPatch() - 1);
+  }
+};
+
+const handleExportPatches = () => {
+  if (!PatchUtils.didLoadPatches()) return;
+
+  const version = PatchUtils.getVersion();
   const result = PatchUtils.exportJsonFromState();
   result.version = version + 1;
 
@@ -22,46 +41,61 @@ const handleLoadPatches = () => {
   const $field = document.getElementById("Toolbar-json");
   const json = $field.value;
   if (json && json.length > 0) {
+    const version = PatchUtils.getVersion();
     const result = PatchUtils.loadJsonToState(json);
     if (result) {
       $field.value = JSON.stringify({
         result: "Load OK",
-        version: State.get("patchesversion")
+        version: version
       });
     }
   }
 };
 
+const renderBankAndPatchButtons = () =>
+  PatchUtils.getVersion()
+    ? [
+        <span className="Toolbar-label">Bank</span>,
+        <input
+          className="Toolbar-field"
+          name="bank"
+          type="text"
+          value={PatchUtils.getBank()}
+          onChange={e => PatchUtils.setBank(e.target.value)}
+          onKeyUp={handleBankKeyUp}
+        />,
+        <span className="Toolbar-label">Patch</span>,
+        <input
+          className="Toolbar-field"
+          name="patch"
+          type="text"
+          value={PatchUtils.getPatch()}
+          onChange={e => PatchUtils.setPatch(e.target.value)}
+          onKeyUp={handlePatchKeyUp}
+        />
+      ]
+    : null;
+
+const renderExportPatchesButton = () =>
+  PatchUtils.getVersion() ? (
+    <button className="Toolbar-button" onClick={handleExportPatches}>
+      Export Patches To JSON
+    </button>
+  ) : null;
+
 const Toolbar = props => (
   <div className="Toolbar">
     <div className="Toolbar-controls">
-      <span className="Toolbar-label">Bank</span>
-      <input
-        className="Toolbar-field"
-        name="bank"
-        type="text"
-        value={State.state.bank}
-        onChange={e => State.set("bank", e.target.value)}
-      />
-      <span className="Toolbar-label">Patch</span>
-      <input
-        className="Toolbar-field"
-        name="patch"
-        type="text"
-        value={State.state.patch}
-        onChange={e => State.set("patch", e.target.value)}
-      />
+      {renderBankAndPatchButtons()}
+      {renderExportPatchesButton()}
       <button className="Toolbar-button" onClick={handleLoadPatches}>
         Load Patches From JSON
-      </button>
-      <button className="Toolbar-button" onClick={handleExportPatches}>
-        Export Patches To JSON
       </button>
     </div>
     <textarea
       className="Toolbar-json"
       id="Toolbar-json"
-      value={State.state.patchJson}
+      value={State.get("patchJson")}
     />
   </div>
 );
